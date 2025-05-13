080c17765323b3e98ff2e9b1ecbb3e350472a33c
Popeye <127945393+Popeye68@users.noreply.github.com>
2025-05-13 10:05:01 +0530

    Update appex_v3.py

--- Extractor/modules/appex_v3.py
+++ Extractor/modules/appex_v3.py
@@ -10,7 +10,7 @@ from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
 from base64 import b64decode
 from bs4 import BeautifulSoup
-log_channel = (-1002456765218)
+log_channel = (-1002607772171)
 
 def decrypt(enc):
     enc = b64decode(enc.split(':')[0])
