0ebc85cfcac7fc41034711dcdc183c2b6806b6e1
Popeye <127945393+Popeye68@users.noreply.github.com>
2025-05-13 09:02:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "20736921"))
 API_HASH = getenv("API_HASH", "42b34442e52dc3e07b3e0783389be8cb")
-BOT_TOKEN = getenv("BOT_TOKEN", "8015663864:AAGLRoTMXkj9Ndq4PL7oKLo0AtaYT68rxCM")
-OWNER_ID = int(getenv("OWNER_ID", "1366730834"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1366730834").split()))
+BOT_TOKEN = getenv("BOT_TOKEN", "7777329402:AAGg2dW9YnScNQRAoYqh2ExpnZHW2Wn3zXg")
+OWNER_ID = int(getenv("OWNER_ID", "1977145189"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1977145189").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://creatorar30:fdINvMPYXYwUyHdq@cluster0.pbaou.mongodb.net/?retryWrites=true&w=majority")
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002456765218"))
@@ -27,14 +27,14 @@ API_ID = int(os.environ.get("API_ID", "20736921"))
 # ------------------------------------------------
 API_HASH = os.environ.get("API_HASH")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8015663864:AAGLRoTMXkj9Ndq4PL7oKLo0AtaYT68rxCM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7777329402:AAGg2dW9YnScNQRAoYqh2ExpnZHW2Wn3zXg")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("BOT_USERNAME")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "1366730834"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1977145189"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1366730834").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1977145189").split()))
 # ------------------------------------------------
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002319636950"))
 # ------------------------------------------------
