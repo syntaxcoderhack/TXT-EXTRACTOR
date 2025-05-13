51cca1917f7817ebf405fc7436bd3435cad4b4e1
Popeye <127945393+Popeye68@users.noreply.github.com>
2025-05-13 09:17:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,44 +1,14 @@
-"""
-from os import getenv
-
-
-API_ID = int(getenv("API_ID", "20736921"))
-API_HASH = getenv("API_HASH", "42b34442e52dc3e07b3e0783389be8cb")
-BOT_TOKEN = getenv("BOT_TOKEN", "7777329402:AAGg2dW9YnScNQRAoYqh2ExpnZHW2Wn3zXg")
-OWNER_ID = int(getenv("OWNER_ID", "1977145189"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1977145189").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://creatorar30:fdINvMPYXYwUyHdq@cluster0.pbaou.mongodb.net/?retryWrites=true&w=majority")
-
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002456765218"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002373511813"))
-
-"""
-#
-
-
-
-
-# --------------M----------------------------------
-
 import os
 from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "20736921"))
-# ------------------------------------------------
-API_HASH = os.environ.get("API_HASH")
-# ----------------D--------------------------------
+
+API_ID = int(os.environ.get("API_ID", "27426550"))  # Replace "123456" with your actual api_id or use .env
+API_HASH = os.environ.get("API_HASH", "994f8e90abd48bad09b64c0bed0e56b7")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7777329402:AAGg2dW9YnScNQRAoYqh2ExpnZHW2Wn3zXg")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "1977145189"))
-# ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1977145189").split()))
-# ------------------------------------------------
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002319636950"))
-# ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL")
-# -----------------------------------------------
-PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1002456765218"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1977145189"))  # Your Telegram user ID
+SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "1977145189").split()))  # Space-separated user IDs
 
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://creatorar30:fdINvMPYXYwUyHdq@cluster0.pbaou.mongodb.net/?retryWrites=true&w=majority")
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002607772171"))  # Telegram channel ID (with -100 prefix)
+
+PREMIUM_LOGS = os.environ.get("PREMIUM_LOGS", "-1002607772171")  # Optional
